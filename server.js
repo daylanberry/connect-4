68e1c188c2c63807621ad07c0ff25ea9a1223485
daylan <daylanberry@gmail.com>
2021-10-21 08:46:41 -0700

    adding to heroku

--- server.js
+++ server.js
@@ -14,9 +14,9 @@ const app = express();
 app.use(express.static(path.resolve(__dirname, "../client/build")));
 
 // // All other GET requests not handled before will return our React app
-// app.get("*", (req, res) => {
-//   res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
-// });
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
+});
 
 app.get("/api", (req, res) => {
   res.send("hello");
